938aead2417d557f76148435a352bcd0c1ac61ae
Alec Pizziferro <alecpizz@gmail.com>
2024-11-13 13:28:11 -0600

    misc changes

--- controllers/spot_controller/spot_controller.py
+++ controllers/spot_controller/spot_controller.py
@@ -1,9 +1,7 @@
 """spot_controller controller."""
 from spot_driver import SpotDriver
-
+from controller import AnsiCodes
 
 spot = SpotDriver()
-spot.move_forward(2)
-spot.turn_right(5)
-spot.turn_left(5)
-spot.move_backward(2)
+
+while spot.step(spot.get_timestep()) != -1:
